32961c010d06b7301f6e220f3f765e1f7fde3175
james weston <james@savannah.io>
2016-08-02 12:09:18 -0400

    Adding support for retrieiving an OAuth access token

---
+++ src/AccessTokenUtil.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace jamesvweston\Utilities;
+
+
+class AccessTokenUtil
+{
+
+    /**
+     * Example value in $headers:   ['Authorization' => 'Bearer TOKEN_GOES_HERE'];
+     * @param   array       $headers
+     * @param   string      $key
+     * @param   string      $tokenType
+     * @param   string      $separator
+     * @return  string|null
+     */
+    public static function getAccessToken($headers, $key = 'Authorization', $tokenType = 'Bearer', $separator = ' ')
+    {
+        $value              = ArrayUtil::get($headers[strtolower($key)]);
+
+        if (is_null($value))
+            return null;
+
+        if (!is_array($value))
+            return null;
+
+        if (sizeof($value) != 1)
+            return null;
+
+
+        $tokenString        = $value[0];
+
+        $tokenArray         = explode($tokenType . $separator, $tokenString);
+
+        if (sizeof($tokenArray) != 2)
+            return null;
+
+        return $tokenArray[1];
+    }
+    
+}
