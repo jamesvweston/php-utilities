d6ee586235d0b39ee957a03a40a636bd501572c8
james weston <james@savannah.io>
2016-09-17 07:42:30 -0400

    Adding DatUtil

---
+++ src/DateUtil.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace jamesvweston\Utilities;
+
+
+class DateUtil
+{
+
+    /**
+     * @param   string      $format     eg YYYY-MM-DD
+     * @param   string      $date
+     * @return  bool
+     */
+    public static function validate($format, $date)
+    {
+        if ($format == 'YYYY-MM-DD')
+        {
+            if (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $date))
+                return true;
+            else
+                return false;
+        }
+        return false;
+    }
+
+}
