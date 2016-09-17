fc13c3c67fe0a0950bcf97e32139990ba4d26acb
james weston <james@savannah.io>
2016-09-17 07:55:37 -0400

    Making sure the input is string

--- src/DateUtil.php
+++ src/DateUtil.php
@@ -13,6 +13,9 @@ class DateUtil
      */
     public static function validate($format, $date)
     {
+        if (!is_string($date))
+            return false;
+
         if ($format == 'YYYY-MM-DD')
         {
             if (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $date))
